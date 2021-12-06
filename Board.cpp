15eca0c17f2403fe73f90d170174a57291164f36
Ryan Jiang <jiangr0421@gmail.com>
2021-12-05 19:24:01 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -96,9 +96,30 @@ struct rc {
 };
 
 void Board::selectRandomCell(int& row, int& col){
-	
-}
+	vector <rc> emptyCells; 
+    
+    for(int i = 0; i < numRows; i++){
+        for(int j = 0; j < numCols; j++){
+            if(panel[i][j] == 0){
+                rc newCell; 
+                newCell.row = i;
+                newCell.col = j;
+                emptyCells.push_back(newCell);
+            }
+        }
+    }
 
+    if (emptyCells.size() > 0){
+        int randNum = rand() % emptyCells.size() - 0;
+        panel[emptyCells.at(randNum).row][emptyCells.at(randNum).col] = 1; 
+        print(); 
+        if(noAdjacentSameValue() || emptyCells.size() == 0){
+            std::cout << "Game over" << std::endl;
+            exit(0);
+        }
+    }	
+}
+/*
 void Board::pressUp(){
 	
 }
@@ -178,3 +199,4 @@ void Board::pressLeft(){
 void Board::pressRight(){
 
 }
+*/
